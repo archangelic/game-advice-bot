3c1a3da5e6cfa7025c928527ac2c924dd05821c2
Michael Hancock <michaelhancock89@gmail.com>
2016-06-17 11:14:02 -0700

    Fixed unicode issues with building the corpus.

--- corpus.py
+++ corpus.py
@@ -7,7 +7,6 @@ response = br.get('http://www.gamefaqs.com/games/rankings')
 systems = []
 for i in response.soup.find_all('a')[:26]:
     ref = i.get('href')
-    # print(x, " ", i.get('href'))
     if not ref.startswith('/games'):
         systems.append(i.get('href') + '/')
 
@@ -25,7 +24,6 @@ for link in topGames:
     faq = []
     for each in resp.soup.find_all('a'):
         if '/faqs/' in str(each.get('href')):
-            # print(each.get('href'))
             faq.append("http://www.gamefaqs.com" + each.get('href'))
     try:
         for each in faq[:8]:
@@ -33,7 +31,11 @@ for link in topGames:
     except:
         pass
 
-badText = ["-", "=", "+", "~", "@", "&", "*", "<", ">", "\\", "/", "#", "`", "|"]
+# Clearing out ASCII Art
+badText = [
+    "-", "=", "+", "~", "@", "&", "*", "<", ">", "\\", "/", "#", "`",
+    "|", "_", ":", "(", ")"
+    ]
 
 for guide in guides:
     resp = br.get(guide)
@@ -42,4 +44,6 @@ for guide in guides:
         guideText = each.get_text()
         for ch in badText:
             guideText = guideText.replace(ch, " ")
-        print(guideText)
+        # Strips out non-ascii characters because they aren't really necessary
+        cleanText = ''.join([i if ord(i) < 128 else ' ' for i in guideText])
+        print(cleanText)
